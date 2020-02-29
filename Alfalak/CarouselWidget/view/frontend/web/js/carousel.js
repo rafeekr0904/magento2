54ffcde3a4be1ae530ce7faaf0df0cdcee78d868
rafeek <rafeekr09@gmail.com>
2020-02-29 18:32:31 +0600

    Module to add owl carousal in frontend

---
+++ Alfalak/CarouselWidget/view/frontend/web/js/carousel.js
@@ -0,0 +1,11 @@
+define([
+'jquery',
+'Alfalak_CarouselWidget/js/carousel/owl.carousel.min'
+],function($){
+	'use strict';
+	$('.owl-carousel').owlCarousel({
+		loop : true,
+		items : 1,
+		nav : true
+		});
+	});
